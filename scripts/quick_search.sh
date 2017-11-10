4cbb0020f709bbed7fb8b315dc32834fadb8f082
Samir M Lalvani <slalvani@terpmail.umd.edu>
2017-11-10 13:38:45 -0500

    Removed unused gem

--- scripts/quick_search.sh
+++ scripts/quick_search.sh
@@ -10,5 +10,4 @@ cd quick_search
 echo ""  >> Gemfile
 echo "# Inserted by bash script"  >> Gemfile
 echo "gem 'quick_search-core', :git => 'https://github.com/samirlalvani/quick_search.git' "  >> Gemfile
-echo "gem 'jquery-rails'"  >> Gemfile
 bundle install
